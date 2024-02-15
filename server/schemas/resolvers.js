dd7f8d8611338055c42455df636e056c4be4ef1b
Jescel James <jesceljames@gmail.com>
2024-02-15 18:01:45 -0500

    resolvers and schema shows up in apollo, but no data yet. Either I create seed or create adduser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,14 +4,24 @@ const { signToken, AuthenticationError } =  require('../utils/auth');
 const resolvers = {
 
   Query: {
+    // By adding context to "me" query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User
+          .findOne({ _id: context.user._id});
+      }
+      throw AuthenticationError;
+    }
+
 
 
   },
 
-  Mutation: {
+
+  // Mutation: {
 
 
-  }
+  // }
 
 
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,18 @@
 const typeDefs =`
 
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    
+
+  }
+
+
+  type Query {
+    me: User
+
+  }
 
 
 `;
