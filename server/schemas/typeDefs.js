35f2ca76423cc49a93628492a5a4643b07171120
Jescel James <jesceljames@gmail.com>
2024-02-15 17:12:29 -0500

    redone index.js in schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,11 +7,6 @@ const typeDefs = `
     savedBooks: [Book]
   }
 
-  type Auth {
-    token: ID!
-    user: User
-  }
-
   type Book {
     bookId: ID!
     authors: [String]
@@ -21,26 +16,20 @@ const typeDefs = `
     link: String
   }
 
-  input InputBook {
-    bookId: String
-    authors: [String]
-    title: String
-    description: String
-    image: String
-    link: String
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
     me: User
   }
 
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(newBook: InputBook!): User
-    removeBook(bookId: ID!): User
-  }
-
+  }  
 
 
 `;
