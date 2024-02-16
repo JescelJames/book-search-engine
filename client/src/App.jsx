1d02079862842012b4318de82d09352a6529f970
Jescel James <jesceljames@gmail.com>
2024-02-16 09:35:42 -0500

    first commit in branch, added apollo imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
